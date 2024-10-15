32194eeab9e0d147eea284b56a50eda4045adb22
SnehalWadekar <snehalwadekar9@gmail.com>
2024-10-15 15:13:23 +0530

    pushOne

---
+++ src/test/java/Demo/SampleTest.java
@@ -0,0 +1,13 @@
+package Demo;
+
+import org.testng.annotations.Test;
+
+public class SampleTest {
+	@Test
+	public void m1()
+	{
+		System.out.println("Hi");
+		//pulling
+	}
+
+}
---
+++ src/test/java/Demo/Sma1Test.java
@@ -0,0 +1,12 @@
+package Demo;
+
+import org.testng.annotations.Test;
+
+public class Sma1Test {
+	@Test
+	public void m2() 
+	{
+		System.out.println("Bye");
+	}
+
+}
